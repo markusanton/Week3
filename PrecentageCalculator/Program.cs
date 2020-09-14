d2e6aaaf3a738ab88b9ed332028050aef8fd02c8
markusanton <markusa070@gmail.com>
2020-09-14 14:42:13 +0300

    add percentage calculator

---
+++ PrecentageCalculator/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace PrecentageCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Maximum;
+            int UserScore;
+            double Result;
+
+            Console.WriteLine("What is your maximum score?");
+            Maximum = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("What is your score?");
+            UserScore = Int32.Parse(Console.ReadLine());
+
+            Result = ((UserScore * 100) / Maximum);
+            Console.WriteLine($"You scored {Result}%");
+        }
+    }
+}
