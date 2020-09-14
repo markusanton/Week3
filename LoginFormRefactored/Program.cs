df3c4ba528a067310146183c32c6c9cdc4a40f5f
markusanton <markusa070@gmail.com>
2020-09-14 14:13:24 +0300

    Add project files.

---
+++ LoginFormRefactored/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace LoginFormRefactored
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string UserName, Password;
+            Console.WriteLine("Enter your username:");
+            UserName = Console.ReadLine();
+
+            Console.WriteLine("Enter your password:");
+            Password = Console.ReadLine();
+
+            if (UserName == "admin" && Password == "pass1234")
+            {
+                Console.WriteLine("Welcome, admin!");
+            }
+            else
+            {
+                Console.WriteLine("Try again.");
+            }
+        }
+    }
+}
---
+++ Week3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Week3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your username: ");
+            string UserName = Console.ReadLine();
+
+            Console.WriteLine("Please enter your password: ");
+            string Password = Console.ReadLine();
+
+            if (UserName == "admin" && Password == "pass1234")
+            {
+                Console.WriteLine("Welcome!");
+            }
+            else if (UserName != "admin")
+            {
+                Console.WriteLine("Incorrect username.");
+            }
+            else if (Password != "pass1234")
+            {
+                Console.WriteLine("Incorrect password.");
+            }
+            else
+            {
+                Console.WriteLine("Please try again."); 
+            }
+        }
+    }
+}
